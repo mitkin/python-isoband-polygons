5bc9ee82f15e68cd994f579fa703ce3e4a0882f6
Mikhail Itkin <itkin.m@gmail.com>
2017-12-01 15:43:33 +0100

    Intermittent commit

--- contours_isfrekvens.py
+++ contours_isfrekvens.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
-import rasterio as rst
-import numpy as np
-from rasterio.features import shapes
-import geopandas as gp
-import pandas as pd
 import os
-from shapely.geometry import Polygon, MultiPolygon
-from rdp import rdp
-from skimage.filters import gaussian
-from matplotlib import pyplot as plt
+
+import geopandas as gp
+import numpy as np
+import pandas as pd
+import rasterio as rst
 from contours.core import shapely_formatter as shapely_fmt
 from contours.quad import QuadContourGenerator
+from skimage.filters import gaussian
 
 
 def get_xy_coords(dst):
@@ -31,29 +28,29 @@ def get_xy_coords(dst):
     except:
         raise
 
-    x = np.linspace(dst.bounds.left + dx/2., dst.bounds.right - dx/2., dst.width)
-    y = np.linspace(dst.bounds.bottom + dy/2., dst.bounds.top - dy/2., dst.height)
+    x = np.linspace(dst.bounds.left + dx / 2., dst.bounds.right - dx / 2., dst.width)
+    y = np.linspace(dst.bounds.bottom + dy / 2., dst.bounds.top - dy / 2., dst.height)
 
     return x, y
 
 
-def _extract_polygon(src_data, dx, dy, minval, maxval, transform=None, src=None):
+def _extract_polygon(src_data, dx, dy, minval, maxval):
     """
     """
-    src_gauss = gaussian(np.where(src_data > minval, 1, 0),
+    src_gauss = gaussian(np.where((src_data > minval)*(src_data<=maxval), 1, 0),
                          sigma=0.75,
                          preserve_range=True)
 
-    # src_gauss = np.ma.array(src_gauss, mask=(src_data==0))
+    c = QuadContourGenerator.from_rectilinear(dx,
+                                              dy,
+                                              np.flipud(src_gauss),
+                                              shapely_fmt)
 
-    x, y = np.meshgrid(dx, dy)
-    c = QuadContourGenerator.from_rectilinear(dx, dy, np.flipud(src_gauss), shapely_fmt)
-
-    contour = c.filled_contour(min=0.354, max=100.0)
+    contour = c.filled_contour(min=0.7, max=2.0)
 
     results = ({'properties': {'value': minval}, 'geometry': s}
                for i, s in enumerate(contour))
-    geoms=list(results)
+    geoms = list(results)
     return geoms
 
 
@@ -66,7 +63,7 @@ def extract_polygons(src_data, levels, dx, dy, transform=None, maxval=101, src=N
 
     for level in levels:
 
-        output_path = 'cntrs-{:02d}.geojson'.format(int(level))
+        output_path = 'cntrs-sigma-7.geojson'.format()
         if os.path.exists(output_path):
             os.remove(output_path)
 
@@ -74,14 +71,11 @@ def extract_polygons(src_data, levels, dx, dy, transform=None, maxval=101, src=N
                                  dx,
                                  dy,
                                  level,
-                                 maxval,
-                                 transform=transform,
-                                 src=src)
+                                 maxval
+                                 )
 
-        gpd_orig = gp.GeoDataFrame.from_features(geoms)
-        gpd_current = gpd_orig
-        gpd_current.crs = {'init': 'epsg:3413'}
-
+        gpd_current = gpd_orig = gp.GeoDataFrame.from_features(geoms)
+        gpd_current.crs = src.crs
 
         if gpd_prev is None:
             gpd_prev = gpd_current
@@ -93,23 +87,26 @@ def extract_polygons(src_data, levels, dx, dy, transform=None, maxval=101, src=N
             gpd_prev = gp_new.copy()
 
     try:
-       gp_new.to_file(output_path, driver='GeoJSON')
+        gp_new.to_file(output_path, driver='GeoJSON')
     except:
-       gpd_current.to_file(output_path, driver='GeoJSON')
+        gpd_current.to_file(output_path, driver='GeoJSON')
 
 
 def main():
-    # src = rst.open('./issmall.tif')
+    # src = rst.open('./issmall.tif')/
     src = rst.open('/home/mikhail/Projects/IsfrekvensPolygons/isfrekvens-masked_epsg3413.tif')
+    # src = rst.open('/home/mikhail/Downloads/isfrekvens-1/icechart_seaicefrequencymap198604_201504_EPSG3575.tif')
+    # src = rst.open('/tmp/islarge.tif')
     dx, dy = get_xy_coords(src)
     src_data = src.read(1)
     levels = [0.1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99.9][::-1]
 
     extract_polygons(src_data,
                      levels,
-                     dx,dy,
+                     dx, dy,
                      transform=src.transform,
                      src=src)
+
 
 if __name__ == "__main__":
     main()
